f3e5348ca34eb16ab56ce7f44342e99968fe97e7
Nikolay Mishin <mi@ya.ru>
2014-01-27 01:23:09 +0400

    тренировка

---
+++ TheNewBoston/src/com/thenewboston/travis/StartingPoint.java
@@ -0,0 +1,23 @@
+package com.thenewboston.travis;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class StartingPoint extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.starting_point, menu);
+        return true;
+    }
+    
+}
