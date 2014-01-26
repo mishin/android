a4116f45f3b5a288f2d2ec595f4afd9a7a48315c
Nikolay Mishin <mi@ya.ru>
2014-01-27 02:30:44 +0400

    супер, кнопки заработали!!

--- TheNewBoston/src/com/thenewboston/travis/StartingPoint.java
+++ TheNewBoston/src/com/thenewboston/travis/StartingPoint.java
@@ -3,13 +3,42 @@ package com.thenewboston.travis;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class StartingPoint extends Activity {
 
+	int counter;
+	Button add, sub;
+	TextView display;
+	
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        counter = 0;
+        add = (Button) findViewById (R.id.bAdd);
+        sub = (Button) findViewById (R.id.bSub);
+        display = (TextView) findViewById (R.id.tvDisplay);
+        add.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				counter++;
+				display.setText("Your total is " + counter);
+			}
+		});
+        sub.setOnClickListener(new View.OnClickListener() {
+			
+    			@Override
+    			public void onClick(View v) {
+    				// TODO Auto-generated method stub
+    				counter--;	
+    				display.setText("Your total is " + counter);
+    			}
+    		});
     }
 
 
